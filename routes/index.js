6b04eb9d1264efbba3f0752e3fc4b9708d8661f7
Yunus Abdulhamid <yunusabdulhameed1@gmail.com>
2026-05-07 21:04:01 +0100

    connection final fixes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const adminRoutes = require('./adminRoutes');
-// const authRoutes = require('./authRoutes'); // Future: for login, etc.
+const authRoutes = require('./authRoutes'); // Add this near the top
 
 router.use('/admin', adminRoutes);
-// router.use('/auth', authRoutes);
+router.use('/auth', authRoutes); // Add this near router.use('/admin'...)
 
 module.exports = router;
